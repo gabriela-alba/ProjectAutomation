aa1a390299b1e10b9ca823f68486e77824c20798
gabriela-alba <gabriela.alba24@gmail.com>
2021-07-24 15:42:23 -0300

    Test cases were added

---
+++ src/test/java/project/classes/LoginGmail.java
@@ -0,0 +1,47 @@
+package project.classes;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginGmail extends PageObject {
+    WebDriverWait wait;
+
+    private By email_box = By.id("identifierId");
+    private By next_button = By.xpath("//*[@id=\"identifierNext\"]");
+    private By pass_box = By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input");
+    private By submit_button = By.xpath("//*[@id=\"passwordNext\"]");
+
+    private static final Logger logger = LogManager.getLogger(LoginGmail.class);
+
+    public LoginGmail(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String email, String password){
+
+        if (email != "" && password != ""){
+            type(email, email_box);
+            click(next_button);
+
+            wait = new WebDriverWait(getDriver(), 5);
+            WebElement passbox = wait.until(ExpectedConditions.visibilityOfElementLocated(pass_box));
+
+            passbox.click();
+            type(password, pass_box);
+            click(submit_button);
+
+            logger.info("Logged with this credentials: ");
+            logger.info("Email: {}", email);
+            logger.info("Password: {}\n", password);
+
+        } else {
+            logger.info("Logged with invalid credentials: ");
+            logger.error("Enter an email or password");
+        }
+    }
+}
---
+++ src/test/java/project/classes/SearchEbay.java
@@ -0,0 +1,42 @@
+package project.classes;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SearchEbay extends PageObject {
+    WebDriverWait wait;
+
+    private By search_box = By.id("gh-ac");
+    private By search_button = By.id("gh-btn");
+    private By item_search = By.xpath("//*[@id=\"srp-river-results\"]/ul/li[1]/div/div[1]/div/a/div/img");
+    private By item_price = By.xpath("//*[@id=\"srp-river-results\"]");
+
+    private static final Logger logger = LogManager.getLogger(SearchEbay.class);
+
+    public SearchEbay(WebDriver driver) {
+        super(driver);
+    }
+
+    public void searchItem(String item){
+        if (item != "") {
+            click(search_box);
+            type(item, search_box);
+            click(search_button);
+            try {
+                wait = new WebDriverWait(getDriver(), 5);
+                WebElement item_result = wait.until(ExpectedConditions.visibilityOfElementLocated(item_search));
+                logger.info("Successful search");
+            } catch (TimeoutException t){
+                logger.error("Failed searching item");
+            }
+        } else {
+            logger.error("Failed searching null item");
+        }
+    }
+}
