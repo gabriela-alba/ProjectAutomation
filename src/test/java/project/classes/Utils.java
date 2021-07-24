e41bd2affdab8b17ebc406332e2256567aa9f96f
gabriela-alba <gabriela.alba24@gmail.com>
2021-07-24 15:35:38 -0300

    Utils class

---
+++ src/test/java/project/classes/Utils.java
@@ -0,0 +1,8 @@
+package project.classes;
+
+public class Utils {
+    public final static String URL_GMAIL = "https://www.gmail.com/";
+
+    public final static String URL_EBAY = "https://www.ebay.com/";
+
+}
